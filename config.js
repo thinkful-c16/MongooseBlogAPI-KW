9c46d282dda459d7cae2d64e3d8267f4246db981
Kayla Webb <kaylarwebb@gmail.com>
2017-11-22 11:53:14 -0500

    added db connection info for the local db.

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://localhost/blog-app';
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL,
+  pool: { min: 0, max: 3},
+  debug: true
+};
+
 exports.PORT = process.env.PORT || 8080; 
